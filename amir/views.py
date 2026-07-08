bb75b3577192bfe9e7fe7f180694afec87565bc1
amir70100 <amiralimosavi1385@gmail.com>
2026-07-08 19:58:52 +0330

    داینامیک سازی popular posts

--- amir/views.py
+++ amir/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import get_object_or_404, render
 from amir.models import post
 
+def get_popular_posts():
+    return post.objects.filter(is_active=True).order_by('-published_at')[:3]
+
 def index(request):
     return render(request, "index.html")
 
 def blog_home(request):
-    posts = post.objects.all()
-    content = {'posts': posts}
+    posts = post.objects.filter(is_active=True).order_by('-published_at')
+    content = {
+        'posts': posts,
+        'popular_posts': get_popular_posts(),
+    }
     return render(request, "blog-home.html", content)
 
 def about(request):
@@ -15,8 +21,11 @@ def about(request):
 def blog_single(request, pid=None):
     selected_post = None
     if pid is not None:
-        selected_post = get_object_or_404(post, id=pid)
-    return render(request, "blog-single.html", {'post': selected_post})
+        selected_post = get_object_or_404(post, id=pid, is_active=True)
+    return render(request, "blog-single.html", {
+        'post': selected_post,
+        'popular_posts': get_popular_posts(),
+    })
 
 def contact(request):
     return render(request, "contact.html")
